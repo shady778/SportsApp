912d2d9b0d29d6893bee8e01b6131d4ebab054f0
Shady <shadyramadan.sr@gmail.com>
2026-05-19 16:13:55 +0300

    First Commit : Setup SportsApp project structure

---
+++ SportsApp/SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Shady Ramadan on 19/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
